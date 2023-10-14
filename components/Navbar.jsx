99b1b9389ae885b62f470f08e2bb47db889f1f36
Hamud <59334729+IamHamud@users.noreply.github.com>
2023-10-14 22:25:26 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,11 +4,29 @@ import React, { useState, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import NavLogo from '../public/assets/navLogo.png';
+// import { useRouter } from 'next/router';
+import NavLogo from '../public/assets/navLogo.png'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  // const [position, setPosition] = useState('fixed')
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   if (
+  //     router.asPath === '/Payments Dashboard' ||
+  //     router.asPath === '/Payments Dashboard' ||
+  //   ) {
+  //     setNavBg('transparent');
+  //     setLinkColor('#ecf0f3');
+  //   } else {
+  //     setNavBg('#ecf0f3');
+  //     setLinkColor('#1f2937');
+  //   }
+  // }, [router]);
 
   const handleNav = () => {
     setNav(!nav);
@@ -27,10 +45,11 @@ const Navbar = () => {
 
   return (
     <div
+      style={{ backgroundColor: `${navBg}` }}
       className={
         shadow
-          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300 bg-white'
-          : 'fixed w-full h-20 z-[100] bg-white'
+          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
+          : 'fixed w-full h-20 z-[100]'
       }
     >
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -46,7 +65,7 @@ const Navbar = () => {
           </a>
         </Link>
         <div>
-          <ul className='hidden md:flex text-#1f2937'>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
             <li className='ml-10 text-sm uppercase hover:border-b'>
               <Link href='/'>Home</Link>
             </li>
@@ -67,45 +86,53 @@ const Navbar = () => {
             </li>
           </ul>
           {/* Hamburger Icon */}
-          <div onClick={handleNav} className='md:hidden text-#1f2937'>
+          <div
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className='md:hidden'
+          >
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
       {/* Mobile Menu */}
+      {/* Overlay */}
       <div
         className={
           nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
         }
       >
+        {/* Side Drawer Menu */}
         <div
           className={
             nav
-              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-#e6e7ec p-10 ease-in duration-500'
+              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
               : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
           }
         >
-          <div className='flex w-full items-center justify-between'>
-            <Link href='/'>
-              <a>
-                <Image src={NavLogo} width='87' height='35' alt='/' />
-              </a>
-            </Link>
-            <div
-              onClick={handleNav}
-              className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
-            >
-              <AiOutlineClose />
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Link href='/'>
+                <a>
+                  <Image
+                    src={NavLogo}
+                    width='87'
+                    height='35'
+                    alt='/'
+                  />
+                </a>
+              </Link>
+              <div
+                onClick={handleNav}
+                className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
+              >
+                <AiOutlineClose />
+              </div>
             </div>
           </div>
-          <div className='border-b border-gray-300 my-4'>
-            <p className='w-[85%] md:w-[90%] py-4'>
-              Lets build something legendary together
-            </p>
-          </div>
           <div className='py-4 flex flex-col'>
-            <ul className='uppercase text-#1f2937'>
+            <ul className='uppercase'>
               <Link href='/'>
                 <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Home
@@ -122,68 +149,67 @@ const Navbar = () => {
                 </li>
               </Link>
               <Link href='/#projects'>
-                <li
-                onClick={() => setNav(false)} className='py-4 text-sm'>
-                Projects
-              </li>
-            </Link>
-            <Link href='/resume'>
-              <li onClick={() => setNav(false)} className='py-4 text-sm'>
-                Resume
-              </li>
-            </Link>
-            <Link href='/#contact'>
-              <li onClick={() => setNav(false)} className='py-4 text-sm'>
-                Contact
-              </li>
-            </Link>
-          </ul>
-          <div className='pt-40'>
-            <p className='uppercase tracking-widest text-[#5651e5]'>
-              Lets Connect
-            </p>
-            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-              <a
-                href='https://www.linkedin.com/in/iamhamud/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <FaLinkedinIn />
-                </div>
-              </a>
-              <a
-                href='https://github.com/IamHamud'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <FaGithub />
-                </div>
-              </a>
-              <Link href='/#contact'>
-                <div
-                  onClick={() => setNav(!nav)}
-                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
-                >
-                  <AiOutlineMail />
-                </div>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Projects
+                </li>
               </Link>
               <Link href='/resume'>
-                <div
-                  onClick={() => setNav(!nav)}
-                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
-                >
-                  <BsFillPersonLinesFill />
-                </div>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Resume
+                </li>
               </Link>
+              <Link href='/#contact'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>
+                Let&#39;s Connect
+              </p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <a
+                  href='https://www.linkedin.com/in/iamhamud/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/IamHamud'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href='/#contact'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href='/resume'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Navbar;
